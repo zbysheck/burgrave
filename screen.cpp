0952111680af7f451c815acec77e9280b5e906c0
zbysheck <zbyshekh@gmail.com>
2015-06-15 22:40:15 +0200

    screen class added

---
+++ screen.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zbyshekh on 6/15/15.
+//
+
+#include "screen.h"
---
+++ screen.h
@@ -0,0 +1,14 @@
+//
+// Created by zbyshekh on 6/15/15.
+//
+
+#ifndef BURGRAVE_SCREEN_H
+#define BURGRAVE_SCREEN_H
+
+
+class screen {
+
+};
+
+
+#endif //BURGRAVE_SCREEN_H
